de6fb03f21fe8ddc07122004ee8ed594667ea72b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-17 16:09:01 +0600

    20_32_create a table-design a form & store table information in the db_s9

--- app/Http/Controllers/Management/TableController.php
+++ app/Http/Controllers/Management/TableController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Management;
 
-use App\Http\Controllers\Controller;
+use App\Table;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TableController extends Controller
 {
@@ -24,7 +25,8 @@ class TableController extends Controller
      */
     public function create()
     {
-        //
+
+        return view('management.createTable');
     }
 
     /**
@@ -35,7 +37,14 @@ class TableController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(['name'=>'required|unique:tables|max:255']);
+        $table = new Table();
+        $table->name= $request->name;
+        $table->save();
+        $request->session()->flash('status','table '. $request->name. ' is created successfully');
+        return redirect('management/table');
+
+
     }
 
     /**
