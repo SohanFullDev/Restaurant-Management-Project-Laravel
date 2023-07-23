efa2becb54c863fe61541417d650957ee6f34184
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-23 14:56:53 +0600

    29_41_select a table in the cashier page_s10

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -8,6 +8,9 @@
     <div class="row justify-content-center py5">
          <div class="col-md-5">
             <button class="btn btn-primary btn-block" id="btn-show-tables">View All Tables</button>
+
+            <div id="selected-table"></div>
+
          </div>
          <div class="col-md-7">
             <nav>
@@ -63,6 +66,15 @@
         });
 
      });
+
+     // detect button table onclick to show table data
+     $("#table-detail").on("click",".btn-table",function(){
+        var SELECTED_TABLE_ID = $(this).data("id");
+        var SELECTED_TABLE_NAME = $(this).data("name");
+        $("#selected-table").html('<br><h3>Table: '+SELECTED_TABLE_NAME+'</h3><hr>');
+
+     });
+
 
 
    });
