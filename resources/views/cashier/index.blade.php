4bdaa7a631acfefefff902e1bcc3f5588c2bf813
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-23 16:55:08 +0600

    30_42_add ordered menus to the cashier page_s10

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -5,11 +5,12 @@
 
 
     <div class="row" id="table-detail"></div>
-    <div class="row justify-content-center py5">
+    <div class="row justify-content-center py-5">
          <div class="col-md-5">
             <button class="btn btn-primary btn-block" id="btn-show-tables">View All Tables</button>
 
             <div id="selected-table"></div>
+            <div id="order-detail"></div>
 
          </div>
          <div class="col-md-7">
@@ -67,15 +68,44 @@
 
      });
 
+     var SELECTED_TABLE_ID = "";
+     var SELECTED_TABLE_NAME = "";
      // detect button table onclick to show table data
      $("#table-detail").on("click",".btn-table",function(){
-        var SELECTED_TABLE_ID = $(this).data("id");
-        var SELECTED_TABLE_NAME = $(this).data("name");
+        SELECTED_TABLE_ID = $(this).data("id");
+        SELECTED_TABLE_NAME = $(this).data("name");
         $("#selected-table").html('<br><h3>Table: '+SELECTED_TABLE_NAME+'</h3><hr>');
 
      });
 
+$("#list-menu").on("click", ".btn-menu", function(){
+    //alert("You click to order a menu");
+    if(SELECTED_TABLE_ID == ""){
+        alert("You need to select a table for the customer first");
+    }else {
+        //alert("You can add menu to the table");
+        var menu_id = $(this).data("id");
+        $.ajax({
+            type: "POST",
+            data:{
+                "_token" : $('meta[name="csrf-token"]').attr('content'),
+                "menu_id": menu_id,
+                "table_id": SELECTED_TABLE_ID,
+                "table_name": SELECTED_TABLE_NAME,
+                "quantity" : 1
+            } ,
+            url: "/cashier/orderFood",
+            success: function(data){
+             $("#order-detail").html(data);
 
+            }
+
+        });
+
+
+    }
+
+});
 
    });
 
