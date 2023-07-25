2ba23e77f5930a37d3d2a6d0ea3c6124acf09c4b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-25 11:45:19 +0600

    32_45_save sale & sale details information_s10

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Cashier;
 
 use App\Menu;
+use App\Sale;
 use App\Table;
 use App\Category;
+use App\SaleDetail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class CashierController extends Controller
 {
@@ -61,7 +64,43 @@ class CashierController extends Controller
     }
 
     public function orderFood(Request $request){
-        return $request->menu_id;
+       // return $request->menu_id;
+       $menu = Menu::find($request->menu_id);
+       $table_id = $request->table_id;
+       $table_name = $request->table_name;
+       $sale = Sale::where('table_id', $table_id)->where('sale_status','unpaid')->first();
+        //if there is no sale for the selected table, create a new sale record
+        if(!$sale){
+            $user = Auth::user();
+            $sale = new Sale();
+            $sale->table_id =  $table_id;
+            $sale->table_name = $table_name;
+            $sale->user_id = $user->id;
+            $sale->user_name = $user->name;
+            $sale->save();
+            $sale_id = $sale->id;
+            //update table status
+            $table = Table::find($table_id);
+            $table->status = "unavailable";
+            $table->save();
+
+        }else{ // if there is a sale on the selected table
+            $sale_id = $sale->id;
+        }
+        // add ordered menu to the sale_details table
+        $saleDetail = new SaleDetail();
+        $saleDetail->sale_id = $sale_id;
+        $saleDetail->menu_id = $menu->id;
+        $saleDetail->menu_name = $menu->name;
+        $saleDetail->menu_price = $menu->price;
+        $saleDetail->quantity = $request->quantity;
+        $saleDetail->save();
+        //update total price in the sales table
+        $sale->total_price = $sale->total_price + ($request->quantity * $menu->price);
+        $sale->save();
+
+        return $sale->total_price; //testing
+
     }
 
 }
