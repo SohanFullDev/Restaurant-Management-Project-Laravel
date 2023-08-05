ea083d7863a9371a3956fbdd5b06e9ba973ff006
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-05 13:55:13 +0600

    47_59_style a receipt by using css_s12

--- resources/views/cashier/showReceipt.blade.php
+++ resources/views/cashier/showReceipt.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Restaurant App - Receipt - SaleID : {{ $sale->id }}</title>
+    <link rel="stylesheet" href="{{ asset('/css/receipt.css') }}" media="all">
+    <link rel="stylesheet" href="{{ asset('/css/no-print.css') }}" media="print">
+
 </head>
 <body>
 <div id="wrapper">
