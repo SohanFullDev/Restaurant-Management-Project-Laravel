8846a68f1496599a878db1ba87023680243b78a5
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-05 11:24:51 +0600

    45_57_display a receipt_s12

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::post('/cashier/deleteSaleDetail','Cashier\CashierController@deleteSaleDet
 
 Route::post('/cashier/confirmOrderStatus','Cashier\CashierController@confirmOrderStatus');
 Route::post('/cashier/savePayment','Cashier\CashierController@savePayment');
+Route::get('/cashier/showReceipt/{saleID}','Cashier\CashierController@showReceipt');
 
 
 Route::resource('management/category','Management\CategoryController');
